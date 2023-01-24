080d5451003d0d34eda5595af58a78d17b9f3715
chagit2022 <chadoutaud.git@outlook.fr>
2023-01-23 21:36:50 -0500

    construction site

--- christopheChadoutaudP/src/app/app-routing.module.ts
+++ christopheChadoutaudP/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PortafolioComponent } from './components/portafolio/portafolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component:HomeComponent},
+    {path:'portafolio', component:PortafolioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
