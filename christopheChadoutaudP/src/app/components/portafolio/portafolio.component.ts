af399bf858e261df0def52393b11ff13a6f04cb8
chagit2022 <chadoutaud.git@outlook.fr>
2023-01-26 22:22:55 -0500

    fin du css skills....

--- christopheChadoutaudP/src/app/components/portafolio/portafolio.component.ts
+++ christopheChadoutaudP/src/app/components/portafolio/portafolio.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class PortafolioComponent {
 
+    constructor(){
+        const btn = document.querySelector('btn');
+        const videoContent = document.querySelector('videoContent');
+        const close = document.querySelector('close');
+
+        btn?.addEventListener('click', () =>{
+            videoContent?.classList.add('show');
+        })
+
+        close?.addEventListener('click', () =>{
+            videoContent?.classList.add('show');
+        })
+
+
+    }
+
+
+
+
+
 }
